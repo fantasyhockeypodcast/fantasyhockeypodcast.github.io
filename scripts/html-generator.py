d7ce6acbe36c855d973e6fd53d9b30077c18f46c
Stakenborg <b.stakenborg@gmail.com>
2018-01-22 16:09:13 -0500

    New Episode

--- scripts/html-generator.py
+++ scripts/html-generator.py
@@ -713,6 +713,8 @@ imagedict = {
     "Yanni Gourde": "http://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3094261.png&w=350&h=254",
     "Jonathan Bernier": "http://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3533.png&w=350&h=254",
     "Trevor Lewis": "http://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3454.png&w=350&h=254",
+    "Timo Meier": "http://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/3899978.png&w=350&h=254",
+    "Samuel Girard": "http://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/4063401.png&w=350&h=254",
 }
 
 new_player_images = {}
